18f7ccd194999feb4aec9ad337a9f049408bd508
Anco Esat <esatcomlekci@gmail.com>
2026-07-17 13:02:13 +0300

    Some more route fixes.

--- public_html/core/app/Http/Controllers/SitemapController.php
+++ public_html/core/app/Http/Controllers/SitemapController.php
@@ -3,6 +3,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 class SitemapController extends Controller {
     public function index() {
-        return response()->view('frontend.sitemap')->header('Content-Type', 'text/xml');
+        $xmlHeader = '<?xml version="1.0" encoding="UTF-8"?>';
+        return response()->view('frontend.sitemap', compact('xmlHeader'))->header('Content-Type', 'text/xml');
     }
 }
